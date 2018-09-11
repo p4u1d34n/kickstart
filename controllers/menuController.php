9df365d0f9524674edcf474f79394232f4182fd7
Paul Dean <hello@pauldean.me>
2018-09-11 18:42:58 +0200

    menu model path

--- controllers/menuController.php
+++ controllers/menuController.php
@@ -3,7 +3,7 @@
   
     function __construct() {
       // we store all the posts in a variable
-      require_once('models/menu/Menu.php');
+      require_once('models/Menu.php');
       $menu = Menu::all();
       require_once('views/menu/menu.php');
     }
