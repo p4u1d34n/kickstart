339c00250c2745eaf0a1f7d7bef8edb9f3289295
Paul Dean <hello@pauldean.me>
2018-09-11 15:45:22 +0200

    env file for DB credentials

--- connection.php
+++ connection.php
@@ -1,18 +1,32 @@
 <?php
   class Db {
 
+    private $dbCredentials = NULL;
+
     private static $instance = NULL;
 
     private function __construct() {
+      $this->getEnv();
       $this->getInstance();
     }
 
     private function __clone() {}
 
+    private static function getEnv() {
+      $env = fopen(".env");
+      if($env) {
+        while(($line=fgets($env)) !== false) {
+          self::$dbCredentials = explode($line,",");
+        }
+      } else {
+        die("create .env file");
+      }
+    }
+
     public static function getInstance() {
       if (!isset(self::$instance)) {
         $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-        self::$instance = new PDO('mysql:host=localhost;dbname=mvc', 'rootuser', 'p455w0rd', $pdo_options);
+        self::$instance = new PDO('mysql:host='.$dbCredentials[0].';dbname='.$dbCredentials[1],$dbCredentials[2],$dbCredentials[3], $pdo_options);
       }
         if (!$self::$instance) {
           echo "DB NOT CONNECTED";
