7be4cc7f01995f521676b756369bf23b5dd5b52a
Paul Dean <hello@pauldean.me>
2018-09-11 17:35:48 +0200

    error

--- views/pages/error.php
+++ views/pages/error.php
@@ -4,9 +4,7 @@
 									<span class="date">April 25, 2017</span>
 									<h1>Oh, wait, thats not right now is it..</h1>
 									<p>I havent had time to finish the site yet, but I did make sure there was an error page.</p>
-								</header>
-								<div class="image main"><img src="images/pic01.jpg" alt="" /></div>
-								<p>Best look somewhere else, sorry.</p>
-                            </section>
+								</header>	
+</section>
                             
 
