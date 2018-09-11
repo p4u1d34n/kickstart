6f630d68fe7352515d49954a44babb526d3a19ed
Paul Dean <hello@pauldean.me>
2018-09-11 18:42:23 +0200

    menu

--- controllers/menuController.php
+++ controllers/menuController.php
@@ -3,6 +3,7 @@
   
     function __construct() {
       // we store all the posts in a variable
+      require_once('models/menu/Menu.php');
       $menu = Menu::all();
       require_once('views/menu/menu.php');
     }
