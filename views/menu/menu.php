8dd41f62276777c77527ece0fe08a28dd37f6ce1
Paul Dean <hello@pauldean.me>
2018-09-11 18:49:14 +0200

    add links from DB

--- views/menu/menu.php
+++ views/menu/menu.php
@@ -1,15 +1,16 @@
-<?php
-    print_r($menu);
-?>
 <nav id="nav">
-						<ul class="links">
-							<li class="active"><a href="/">Home</a></li>
-							<li><a href="?controller=posts&action=index">Posts</a></li>
-						</ul>
-						<ul class="icons">
-							<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
-							<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
-							<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
-							<li><a href="#" class="icon fa-github"><span class="label">GitHub</span></a></li>
-						</ul>
-					</nav>
+	<ul class="links">
+<?php
+    foreach ($menu as $item) {
+        echo '<li class=""><a href="'.$item->link.'">'.$item->title.'</a></li>';
+    }
+?>
+    <li><a href="?controller=posts&action=index">Posts</a></li>
+	</ul>
+	<ul class="icons">
+        <li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+        <li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+        <li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+        <li><a href="#" class="icon fa-github"><span class="label">GitHub</span></a></li>
+    </ul>
+</nav>
