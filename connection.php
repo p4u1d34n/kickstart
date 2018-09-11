16a7bd4eb93001340ad103883e93697117597a32
Paul Dean <hello@pauldean.me>
2018-09-11 16:59:01 +0200

    remove env loader

--- connection.php
+++ connection.php
@@ -3,8 +3,8 @@
 
     protected static $dbHost = 'localhost';
     protected static $dbScheme = 'mvc';
-    protected static $dbUser = NULL;
-    protected static $dbPass = NULL;
+    protected static $dbUser = 'rootuser';
+    protected static $dbPass = 'p455w0rd';
     
     private static $instance = NULL;
 
@@ -14,33 +14,6 @@
     }
 
     private function __clone() {}
-
-    private static function getEnv() {
-      
-      try {
-        $env = fopen(".db");
-        if($env) {
-          $credentials = array();
-          while(($line=fgets($env)) !== false) {
-            $credentials = explode(",",$line);
-            print_r($credentials);
-          }
-          if( count($credentials) == 0 ) {
-            throw new Exception('Nothing in credentials');
-          } else {
-            self::$dbUser = $credentials[0];
-            self::$dbPass = $credentials[1];
-          }
-
-        } else {
-          throw new Exception('Did not open env.ini');
-        }
-      } catch(Exception $e) {
-        print "didnt load env.ini\n<pre>";
-        print_r($e) . '</pre>';
-      }
-      
-    }
 
     public static function getInstance() {
       if (!isset(self::$instance)) {
