67ebeb71aab91621e0899ae36506e0cf3a8a9f88
Paul Dean <hello@pauldean.me>
2018-09-11 19:02:15 +0200

    fix ? on qs

--- index.php
+++ index.php
@@ -3,8 +3,6 @@
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
-print_r($_SERVER["QUERY_STRING"]);
-
   require_once('connection.php');
 
   if (isset($_GET['controller']) && isset($_GET['action'])) {
