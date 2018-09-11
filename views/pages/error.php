5eeaad4b9d14c620f6eed12ad15889e0ce59e621
Paul Dean <hello@pauldean.me>
2018-09-11 17:37:36 +0200

    error

--- views/pages/error.php
+++ views/pages/error.php
@@ -1,9 +1,9 @@
 
 <section class="post">
 								<header class="major">
-									<span class="date">You walked right into a 404</span>
-									<h1>Oh, wait, thats not right now is it..</h1>
-									<p>I havent had time to finish the site yet, but I did make sure there was an error page.</p>
+									<span class="date">Danger Will Robinson!</span>
+									<h1>404</h1>
+									<p>I havent had time to finish the site yet, but I did make sure there was a 404 page.</p>
 								</header>	
 </section>
                             
