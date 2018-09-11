8a47d065e4ce0b5778d6846317aa6b694e0d1b58
Paul Dean <hello@pauldean.me>
2018-09-11 17:36:47 +0200

    error

--- views/pages/error.php
+++ views/pages/error.php
@@ -1,7 +1,7 @@
 
 <section class="post">
 								<header class="major">
-									<span class="date">April 25, 2017</span>
+									<span class="date">You walked right into a 404</span>
 									<h1>Oh, wait, thats not right now is it..</h1>
 									<p>I havent had time to finish the site yet, but I did make sure there was an error page.</p>
 								</header>	
