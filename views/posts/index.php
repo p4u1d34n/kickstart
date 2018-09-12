d4c08ac25dab108b83f0b2babee1b96adccd5757
Paul Dean <hello@pauldean.me>
2018-09-12 14:54:02 +0200

    page linking

--- views/posts/index.php
+++ views/posts/index.php
@@ -5,10 +5,10 @@
 										<span class="date">April 24, 2017</span>
 										<h2><?php echo $post->author; ?></h2>
 									</header>
-									<a href='?controller=posts&action=show&id=<?php echo $post->id; ?>'  class="image fit"><img src="images/pic02.jpg" alt="" /></a>
+									<a href='/posts/show/<?php echo $post->id; ?>'  class="image fit"><img src="/images/pic02.jpg" alt="" /></a>
 									<p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis magna etiam.</p>
 									<ul class="actions special">
-										<li><a href='?controller=posts&action=show&id=<?php echo $post->id; ?>' class="button">Full Story</a></li>
+										<li><a href='/posts/show/<?php echo $post->id; ?>' class="button">Full Story</a></li>
 									</ul>
 								</article>
 <?php } ?>
--- views/posts/show.php
+++ views/posts/show.php
@@ -5,7 +5,7 @@
 <h2><?php echo $post->author; ?></h2>
 <p><?php echo $post->content; ?></p>
 </header>
-<a href="#" class="image main"><img src="images/pic01.jpg" alt="" /></a>
+<a href="#" class="image main"><img src="/images/pic01.jpg" alt="" /></a>
 <ul class="actions special">
 <li><a href="#" class="button large">Full Story</a></li>
 </ul>
